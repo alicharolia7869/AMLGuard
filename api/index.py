6226da29cc73075448d75862fab76ce6fb73512c
alicharolia7869 <alicharolia7869@gmail.com>
2026-08-12 15:01:49 +0530

    Add custom 404 errorhandler in api/index.py for path debugging

--- api/index.py
+++ api/index.py
@@ -10,29 +10,7 @@ from app import create_app
 
 app = create_app()
 
-class VercelWSGIHandler:
-    def __init__(self, flask_app):
-        self.flask_app = flask_app
-
-    def __call__(self, environ, start_response):
-        # Extract actual edge requested URL path from Vercel headers
-        forwarded = environ.get('HTTP_X_FORWARDED_URI') or environ.get('HTTP_X_MATCHED_PATH')
-        if forwarded:
-            path = forwarded.split('?')[0]
-        else:
-            path = environ.get('PATH_INFO', '/')
-            if path.startswith('/api/index.py'):
-                path = path[13:]
-            elif path.startswith('/api/index'):
-                path = path[10:]
-            elif path.startswith('/api'):
-                path = path[4:]
-                
-        if not path or path == '':
-            path = '/'
-            
-        environ['PATH_INFO'] = path
-        environ['SCRIPT_NAME'] = ''
-        return self.flask_app(environ, start_response)
-
-app.wsgi_app = VercelWSGIHandler(app.wsgi_app)
+@app.errorhandler(404)
+def custom_404(e):
+    from flask import request
+    return f"DEBUG 404: request.path={request.path} | request.url={request.url} | PATH_INFO={request.environ.get('PATH_INFO')} | RAW_URI={request.environ.get('RAW_URI')} | HTTP_X_FORWARDED_URI={request.environ.get('HTTP_X_FORWARDED_URI')}", 404
